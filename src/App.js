b9e56971a0603baffd940f054d3a9297b9bb69be
Alejandro.Rodriguez <Alejandro.Rodriguez@PROPIACLIP060s-MacBook-Pro-2087.local>
2018-06-24 17:53:50 -0500

    created components folder, user component is under construction

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Git Hub Search</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          we are working on it ...
         </p>
       </div>
     );
