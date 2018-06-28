00b177c6e087fdb03af3a01eb76995b9439eface
Alejandro.Rodriguez <Alejandro.Rodriguez@PROPIACLIP060s-MacBook-Pro-2087.local>
2018-06-28 15:59:03 -0500

    starting graphql  queries

--- server/config.js
+++ server/config.js
@@ -1,9 +1,12 @@
 const express = require ('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
 const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
