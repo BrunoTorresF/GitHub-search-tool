bf35c04948be9c7359d4c5ac7345ecf494e20a84
AlejandroRodriguezP <a.rodriguez.p.1995@gmail.com>
2018-07-09 15:00:56 -0500

    get request working, tested on postman

--- server/config.js
+++ server/config.js
@@ -1,40 +1,22 @@
 const express = require ('express');
 const bodyParser = require("body-parser");
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
-const app = express()
+const app = express();
+const request = require('request');
 //Requierements and app are declared above.
 //'16000289fc7a2f6fec7f6589838045855198864a'
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-
+app.get('/search/users', function(req, res) {
+    request('https://api.github.com/search/users?q=location:mexico+language:javascript', function(error, response, body) {
+        res.json(body)
+    });
+});
 
 
-`);
-//user is going to  search for users by location and/or language.
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-
-
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log(`Listening to Port ${port}!!!`);
